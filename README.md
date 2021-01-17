ca42d6a0b06115b7951191ece211c3a6ec0fcc6d
yuanjiawei <957089263@qq.com>
2021-01-17 23:33:03 +0800

    update

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ elasticsearch学习demo
 ```shell script
 docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.10.2
 ```
+
+2. 搭建kibana服务
+```shell script
+docker run --link YOUR_ELASTICSEARCH_CONTAINER_NAME_OR_ID:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:7.10.2
+```
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+require_once "vendor/autoload.php";
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+
+
+$elasticSearchHost = 'http://127.0.0.1:9200';
+
+// list
+$res = $client->get($elasticSearchHost . '/_cat/indices?v');
+var_dump($res->getBody()->getContents());
+
+// create index
+//$res1 = $client->put($elasticSearchHost . '/customer?pretty');
+//var_dump($res1->getBody()->getContents());
+
+// put something into index
+//$res2 = $client->put($elasticSearchHost . '/customer/doc/1?pretty', [
+//    'headers' => [
+//        'Content-Type' => 'application/json',
+//    ],
+//    'body' => '{"name":"John Doe1"}',
+//    'http_errors' => false,
+//    ]);
+//var_dump($res2->getBody()->getContents());
+
+
+// see the document
+//$res3 = $client->get($elasticSearchHost . '/customer/doc/1?pretty');
+//var_dump($res3->getBody()->getContents());
+
+// delete index
+$res4 = $client->delete($elasticSearchHost . '/customer?pretty');
+var_dump($res4->getBody()->getContents());
+
+// see list again
+$res5 = $client->get($elasticSearchHost . '/_cat/indices?v');
+var_dump($res5->getBody()->getContents());
+
